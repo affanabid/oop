64726f6e996fe75853dba7882ebdc7e969d2cab1
Harry <affan.abid99@gmail.com>
2023-10-30 20:14:25 +0500

    n

--- prob/check.py
+++ prob/check.py
@@ -1,3 +1,6 @@
 from itertools import permutations, combinations, combinations_with_replacement
-print(list(permutations(['a','b','c'], 2)))
-print(list(combinations_with_replacement(['a','b'], 2)))
+print(list(permutations(['a','b', 'c'], 3)))
+# print(list(combinations(['a','b','c'], 2)))
+
+print(list(combinations_with_replacement(['a','b', 'c'], 3)))
+print(list(combinations(['a','b', 'c'], 2)))
