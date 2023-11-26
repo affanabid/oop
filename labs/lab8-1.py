6b749cf5806582f4eb47db73da0eb00236ddcb5b
Harry <affan.abid99@gmail.com>
2023-11-26 09:49:40 +0500

    lab8 complete

---
+++ labs/lab8-1.py
@@ -0,0 +1,37 @@
+file1 = open("grades.txt")
+file2 = open("errors.txt", "w")
+
+s = file1.readline()   # read and skip first header line
+file2.writelines(s)
+s = file1.readline()   # read and skip second header line
+file2.writelines(s)
+
+errs = 0
+lineno = 3
+s = file1.readline()   # read first record line
+while s != "":
+    rollno = s[0:10]
+    name = s[10:41]
+    sess = s[52:54]
+    if len(s) != 58:
+        errs += 1
+        file2.writelines(str(lineno)+"\n")
+        file2.writelines(s)   
+    elif len(rollno.strip()) != 10:
+            errs += 1
+            file2.writelines(str(lineno)+"\n")
+            file2.writelines(s)
+    elif not (sess[0] >= '0' and sess[0] <= '9' and sess[1] >= '0' and sess[1] <= '9'):
+            errs += 1
+            file2.writelines(str(lineno)+"\n")
+            file2.writelines(s)
+    lineno += 1
+    s = file1.readline()   # read next record line
+    
+file1.close()
+file2.close()
+
+if errs == 0:
+    print("grades file is error FREE")
+else:
+    print(errs, "errors are present in grades file")
---
+++ labs/lab8-2.py
@@ -0,0 +1,14 @@
+with open("errors.txt") as file1:
+    with open("grades.txt", "r+") as file2:
+        file1.readline()
+        file1.readline()
+        
+        line = file1.readline()
+        while line != "":
+            lineno = int(line)
+            line = file1.readline()  # read next record line
+            file2.seek((lineno-1)*59)
+            file2.writelines(line)
+            line = file1.readline()
+
+print("Update done")
